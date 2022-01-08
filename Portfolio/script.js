f8caa904634894dde95a3116ac0cbd01d86621ae
New Macbook <newmac@News-MacBook-Pro.local>
2022-01-08 20:32:53 +0100

    First commit

---
+++ Portfolio/script.js
@@ -0,0 +1,23 @@
+function openForm1() {
+    document.getElementById("myForm1").style.display = "block";
+}
+
+function closeForm1() {
+    document.getElementById("myForm1").style.display = "none";
+}
+
+function openForm2() {
+    document.getElementById("myForm2").style.display = "block";
+}
+
+function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+}
+
+function openMenu() {
+    let x = document.getElementById("hamburger");
+    x.classList.toggle('show');
+}
+
+
+
